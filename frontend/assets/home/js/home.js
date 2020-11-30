25e3151c00eb67be04e7c11923a21447e198f6be
Kamil <kamiljaved98@gmail.com>
2020-11-30 19:40:26 +0500

    30-11-2020_3

--- frontend/assets/home/js/home.js
+++ frontend/assets/home/js/home.js
@@ -7,6 +7,7 @@
 
 const sdbrL = document.querySelector(".sidebar.left")
 const sdbrL_ddws = document.querySelectorAll(".opt-dropdown-wrap")
+const sdbrR = document.querySelector(".sidebar.right")
 
 sdbrL_ddws.forEach(el => {
     const opt = el.querySelector(".opt")
@@ -26,17 +27,49 @@ accSpeakers.forEach(el => {
     }
 });
 
-const btnCreateProject = document.getElementById("create_new_project");
 const mainContent = document.querySelector(".main-content");
+
+const btnLeftSidebarShowHide = document.getElementById("ls_show_hide");
+
+hideRightSidebar = () => {
+    if (!sdbrR.classList.contains("closed")) sdbrR.classList.add("closed");
+}
+
+showRightSidebar = () => {
+    if (sdbrR.classList.contains("closed")) sdbrR.classList.remove("closed");
+}
+
+toggleLeftSidebar = () => {
+    if (!sdbrL.classList.contains("collapsed")) sdbrL.classList.add("collapsed");   // this class must be present for this functionality
+    if (sdbrL.classList.contains("show")) { 
+        sdbrL.classList.remove("show");
+        showRightSidebar();
+    } else {
+        sdbrL.classList.add("show");
+        hideRightSidebar();
+    }
+}
+
+btnLeftSidebarShowHide.onclick = toggleLeftSidebar
 
 collapseLeftSidebar = () => {
     if (!sdbrL.classList.contains("collapsed")) sdbrL.classList.add("collapsed");    
     if (mainContent.classList.contains("small")) mainContent.classList.remove("small");    
+
+    if (sdbrL.classList.contains("show")) { 
+        sdbrL.classList.remove("show");
+    }
+    showRightSidebar();
 }
 
 expandLeftSidebar = () => {
     if (sdbrL.classList.contains("collapsed")) sdbrL.classList.remove("collapsed");    
     if (!mainContent.classList.contains("small")) mainContent.classList.add("small");
+
+    if (sdbrL.classList.contains("show")) { 
+        sdbrL.classList.remove("show");
+    }
+    showRightSidebar();
 }
 
 hidePage = (sel, dir) => {
@@ -51,10 +84,10 @@ showPage = (sel, dir) => {
     $(sel).removeClass('hidden').css('transform', 'translateX(0%)').css('opacity', '1')
 }
 
-const sdbrR_header = document.querySelector(".sidebar.right .side-header");
+const sdbrR_header = sdbrR.querySelector(".side-header");
 const $sdbrR_header = $('.sidebar.right .side-header')
 
-const sdbrR_content = document.querySelector(".sidebar.right .side-content");
+const sdbrR_content = sdbrR.querySelector(".side-content");
 const $sdbrR_content = $('.sidebar.right .side-content')
 
 setRightSidebarContent = (h_html, c_html) => {
@@ -72,9 +105,19 @@ setRightSidebarContent = (h_html, c_html) => {
     }
 }
 
+const btnCreateProject = document.getElementById("create_new_project");
 const btnBack_CreateProjectPage_1_Name = document.getElementById("btn_back_crprojName");
 
+disable_btnOpt = (btn) => {
+    if (!btn.classList.contains("sel")) btn.classList.add("sel");
+}
+
+enable_btnOpt = (btn) => {
+    if (btn.classList.contains("sel")) btn.classList.remove("sel");    
+}
+
 btnCreateProject.onclick = () => {
+    disable_btnOpt(btnCreateProject)
     collapseLeftSidebar()
     hidePage('.page-wrap.pg0-wrap')
     showPage('.page-wrap.pg1-wrap')
@@ -82,6 +125,7 @@ btnCreateProject.onclick = () => {
 }
 
 btnBack_CreateProjectPage_1_Name.onclick = () => {
+    enable_btnOpt(btnCreateProject)
     expandLeftSidebar()
     hidePage('.page-wrap.pg1-wrap', 'right')
     showPage('.page-wrap.pg0-wrap')
